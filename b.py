3a27e06d764af677c7d8aea3e8816260210e90ba
abdullah.dar <darabdullah380@gmail.com>
2024-09-19 19:56:47 +0500

    Files updated successfully.

--- b.py
+++ b.py
@@ -11,3 +11,9 @@ def askAI():
    'prompt': [
      {"role": "system", "content": "You have to analyse the user prompt and suggest them more than 5 countries based on their preferences. you only have to suggest them countries based on their preferences and write the standard Name for the countries. You Have to Follow a specific format to suggest them countries in all cases no exception. The format is: [country Name1, Country Name2, Country Name3, ...., country Name N]"},
      {"role": "user", "content": f"{prompt}"}
+    ],
+      "temperature":0.5,
+      "topP":0.3,
+      "lengthPenality":0.3,
+       "maxTokens": 2000
+    },
