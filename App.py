eee84d0dcd962e3950ba4a253d992f53b3f2092d
abdullah.dar <darabdullah380@gmail.com>
2024-09-20 17:04:18 +0500

    Files updated successfully.

--- App.py
+++ App.py
@@ -11,4 +11,13 @@ from tkGIF import gifplay
 
 r = 0
 
-def
+def geo_code(place):
+    url = f"https://api.geoapify.com/v1/geocode/search?text={place}&limit=1&type=country&format=json&apiKey=d76f029b27e04a9cb47a5356a7bf2a87"
+    response = requests.get(url)
+
+    response = response.json()
+    latitude = response['results'][0]['lat']
+    longitude = response['results'][0]['lon']
+
+    return latitude, longitude
+
