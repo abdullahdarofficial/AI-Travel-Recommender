483b5c37ef4ba70976564b5f3e20de662574d7b8
abdullah.dar <darabdullah380@gmail.com>
2024-09-21 18:33:04 +0500

    Files updated successfully.

---
+++ preprocessing.py
@@ -0,0 +1,10 @@
+import pandas as pd
+import random
+
+
+#data = pd.read_csv('world-cities.csv')
+#df = pd.read_csv('world-countries.csv')
+# print(data)
+
+# Add a new column with random values
+# data["avg temp"] = [random.randint(75, 150) for _ in range(len(data))]
