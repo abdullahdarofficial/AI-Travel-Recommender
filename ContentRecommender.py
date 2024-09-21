deba43a23b796aa9b1ac5bd58521dfafb075c334
abdullah.dar <darabdullah380@gmail.com>
2024-09-21 18:27:03 +0500

    Files updated successfully.

---
+++ ContentRecommender.py
@@ -0,0 +1,6 @@
+import pandas as pd
+import numpy as np
+from sklearn.feature_extraction.text import TfidfVectorizer, CountVectorizer
+from sklearn.metrics.pairwise import cosine_similarity
+from time import sleep as s
+import warnings
