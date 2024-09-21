6eb0c7aa6019b22321eb13ec6497e28536956463
abdullah.dar <darabdullah380@gmail.com>
2024-09-21 18:27:16 +0500

    Files updated successfully.

--- ContentRecommender.py
+++ ContentRecommender.py
@@ -4,3 +4,11 @@ from sklearn.feature_extraction.text import TfidfVectorizer, CountVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
 from time import sleep as s
 import warnings
+
+warnings.simplefilter(action='ignore', category=FutureWarning)
+
+class ContentBaseRecommender:
+
+    def __init__(self, data_file='world-countries.csv', wait_time=0.1):
+        self.data = pd.read_csv(data_file)
+        self.wait = wait_time
